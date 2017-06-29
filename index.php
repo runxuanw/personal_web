febc2818eeee8beb4a0562ee0a422c6c14253c27
runxuanw <runxuanwei@gmail.com>
2017-06-28 20:26:14 -0700

    initial commit for personal web

---
+++ index.php
@@ -0,0 +1,422 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width"></meta>
+<title>Runxuan Wei</title>
+<meta name="description" content="Runxuan Wei's personal website. Front-end developer, full-stack developer and general software engineer."/>
+<meta name="keywords" content="Runxuan Wei, runxuan, wei, runxuanwei, runxuan wei, front-end, full-stack, software engineer" />
+<meta name="author" content="Runxuan Wei" />
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="sweetalert/dist/sweetalert.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/form.css">
+<link rel="stylesheet" type="text/css" href="animate_it/css/animations.css">
+<link rel="stylesheet" media="screen" href="css/timeline.css">
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/jquery.color.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="sweetalert/dist/sweetalert.min.js"></script>
+<script src="js/util.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="navbar-header">
+	  <a class="navbar-brand" href="#top" onclick="return navClick('#top');">RUNXUAN WEI</a>
+	</div>
+	<div id="navbar" class="navbar-collapse collapse">
+	  <ul class="nav navbar-nav navbar-right">
+		<li><a href="#intro" onclick="return navClick('#intro');">Intro</a></li>
+		<li><a href="#skill" onclick="return navClick('#skill');">Skill</a></li>
+		<li><a href="#project" onclick="return navClick('#project');">Project</a></li>
+		<li><a href="#experience" onclick="return navClick('#experience');">Experience</a></li>
+		<li><a href="#contact" onclick="return navClick('#contact');">Contact</a></li>
+	  </ul>
+	</div>
+</nav>
+
+<div id="top"></div>
+
+<div class="content">
+	<div id="img_container">
+		<div class="night_parallax"></div>
+		<a href="#intro" id="frontLink" onclick="return navClick('#intro');">Hello World..<span class="blink">.</span></a>
+	</div>
+	
+<div id="intro" class="section">
+
+<h2 align="center">Introduction</h2>
+<br><br><br><br>
+
+<div class="inline-wrapper clearfix">
+<div id="myPic" class="animatedParent animateOnce" data-appear-top-offset="-100">
+<div class="animated bounceInLeft slow">
+  <div class="bottom circle-img img-circle">
+  <img src="image/person2.png" />
+  </div>
+  <div class="top circle-img img-circle">
+  <img src="image/shadow2.png" />
+  </div>
+</div>
+</div>
+
+
+<div id="introText" class="animatedParent animateOnce">
+	<p class="animated bounceInRight slow">
+		My name is Runxuan Wei. Currently a <b>new graduate student</b> in computer science major from <b>University of Southern California</b>. I love to solve <b>algorithm problems</b> and want to understand things from multiple perspectives. My passion is to build products and see people enjoy using them. My career goal is to become a software architecture in future. 
+		<br>
+		<br>Additionally, I do love to play video games... a lot.
+	</p>
+</div>
+
+</div>
+
+</div>
+
+<div id="skill" class="section">
+<h2>Skill Sets</h2>
+
+<div class="skills-wrapper col-xs-8 animatedParent animateOnce" data-sequence="500">
+<div class="row animated fadeInUp slow" data-id="1">
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/java.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/angularJS.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/aws.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/javascript.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/android-studio.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/android.png"></img>
+</div>
+
+</div><div class="row animated fadeInUp slow" data-id="2">
+
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/html.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/mysql.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/jquery.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/css.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/drupal.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/php.png"></img>
+</div>
+
+</div><div class="row animated fadeInUp slow" data-id="3">
+
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/eclipse.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/bootstrap.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/python.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/linux.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/github.png"></img>
+</div>
+<div class="col-xs-2 icon-wrapper">
+<img class="skill_icon img-responsive" src="image/wordpress.png"></img>
+</div>
+
+</div>
+</div>
+</div>
+<div id="project" class="section">
+<h2>Projects</h2>
+<div class="row animatedParent animateOnce" data-appear-top-offset="-300" data-sequence="500">
+<div class="col-xs-4 project_container animated fadeInUp slow" data-id="1">
+<div class="hovereffect">
+	<a class="cm-overlay" href="https://github.com/runxuanw/name_battle">
+		<img src="image/name_battle.png" class="img-responsive shadow">
+		<div class="overlay">
+			<h4>Name Battle</h4>
+		</div>
+	</a>
+</div>
+	<img src="image/java2.png" class="project_icon"></img>
+	<img src="image/eclipse.png" class="project_icon"></img>
+	<img src="image/idea.png" class="project_icon"></img>
+</div>
+
+<div class="col-xs-4 project_container animated fadeInUp slow" data-id="2">
+<div class="hovereffect">
+	<a class="cm-overlay" href="http://cs571wrx.us-west-1.elasticbeanstalk.com/hw8.html">
+		<img src="image/search_web.png" class="img-responsive shadow">
+		<div class="overlay">
+			<h4>Facebook Search Web</h4>
+		</div>
+	</a>
+</div>
+
+	<img src="image/angularJS.png" class="project_icon"></img>
+	<img src="image/jquery.png" class="project_icon"></img>
+	<img src="image/html.png" class="project_icon"></img>
+	<img src="image/javascript.png" class="project_icon"></img>
+	<img src="image/php2.png" class="project_icon"></img>
+	<img src="image/bootstrap2.png" class="project_icon"></img>
+	<img src="image/css.png" class="project_icon"></img>
+	<img src="image/aws.png" class="project_icon"></img>
+</div>
+
+<div class="col-xs-4 project_container animated fadeInUp slow" data-id="3">
+<div class="hovereffect">
+	<a class="cm-overlay" href="https://github.com/runxuanw/fb_search_app/tree/master/src">
+		<img src="image/search_app.png" class="img-responsive shadow">
+		<div class="overlay">
+			<h4>Facebook Search App</h4>
+		</div>
+	</a>
+</div>
+	<img src="image/java2.png" class="project_icon"></img>
+	<img src="image/android.png" class="project_icon"></img>
+	<img src="image/android-studio.png" class="project_icon"></img>
+</div>
+
+</div>
+<div class="row animatedParent animateOnce" data-appear-top-offset="-300" data-sequence="500">
+
+
+<div class="col-xs-4 project_container animated fadeInUp slow" data-id="1">
+<div class="hovereffect">
+	<a class="cm-overlay" href="http://foodiedeliver.com/">
+		<img src="image/connect4.png" class="img-responsive shadow">
+		<div class="overlay">
+			<h4>Food I Eat</h4>
+		</div>
+	</a>
+</div>
+	<img src="image/wordpress.png" class="project_icon"></img>
+	<img src="image/php.png" class="project_icon"></img>
+	<img src="image/mysql.png" class="project_icon"></img>
+	<img src="image/html.png" class="project_icon"></img>
+</div>
+
+
+<div class="col-xs-4 project_container animated fadeInUp slow" data-id="2">
+<div class="hovereffect">
+	<a class="cm-overlay" href="https://github.com/runxuanw/connect4_libgdx">
+		<img src="image/connect4.png" class="img-responsive shadow">
+		<div class="overlay">
+			<h4>Connect Four</h4>
+		</div>
+	</a>
+</div>
+	<img src="image/java2.png" class="project_icon"></img>
+	<img src="image/eclipse.png" class="project_icon"></img>
+	<img src="image/libgdx.png" class="project_icon"></img>
+</div>
+
+
+<div class="col-xs-4 project_container animated fadeInUp slow" data-id="3">
+<div class="hovereffect">
+	<a class="cm-overlay" href="https://github.com/runxuanw/ML_kernel">
+		<img src="image/sklearn.png" class="img-responsive shadow">
+		<div class="overlay">
+			<h4>Kernel Transformation</h4>
+		</div>
+	</a>
+</div>
+	<img src="image/python.png" class="project_icon"></img>
+	<img src="image/eclipse.png" class="project_icon"></img>
+	<img src="image/scikit.png" class="project_icon"></img>
+</div>
+
+</div>
+</div>
+<div id="experience" class="section">
+
+	<div class="container">
+		<!-- section title -->
+		<div class="section-title">
+			<h2>Experience</h2>
+			<span class="border"></span>
+		</div>
+		<!-- section title end-->
+		
+		<div class="row">
+			<div class="col-md-12">
+				<div class="timeline">
+
+				<!-- Experience Timeline -->
+
+					<div class="timeline-category exp-category">
+						<a class="large bt-timeline">Experiences</a>
+						<div class="timeline-category-icon">
+							<div class="iconspace"><i class="fa-folder-open"></i></div>
+						</div> <!-- /.timeline-category-icon end -->
+					</div> <!-- /.timeline-category end -->
+						<div class="timeline-post timeline-post-left">											
+							<div class="timeline-post-content-holder">
+								<div class="timeline-post-icon"></div>
+								<div class="timeline-post-title">
+									<h4>SENIOR DEVELOPER 1</h4>
+								</div>
+								<div class="timeline-post-subtitle">
+									<p><span>Company Name </span><span class="timeline-duration">2012 - Current</span></p>
+								</div>
+								<div class="timeline-post-content">
+									<p>Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.</p>
+								</div>
+							</div> <!-- /.timeline-post-content-holder end -->							
+						</div> <!-- /.timeline-post end -->
+						<div class="timeline-post timeline-post-right">											
+							<div class="timeline-post-content-holder">
+								<div class="timeline-post-icon"></div>
+								<div class="timeline-post-title">
+									<h4>SENIOR DEVELOPER 2</h4>
+								</div>
+								<div class="timeline-post-subtitle">
+									<p><span>Company Name </span><span class="timeline-duration">2012 - Current</span></p>
+								</div>
+								<div class="timeline-post-content">
+									<p>Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.</p>
+								</div>
+							</div> <!-- /.timeline-post-content-holder end -->							
+						</div> <!-- /.timeline-post end -->									
+						<div class="timeline-post timeline-post-left">											
+							<div class="timeline-post-content-holder">
+								<div class="timeline-post-icon"></div>
+								<div class="timeline-post-title">
+									<h4>SENIOR DEVELOPER 3</h4>
+								</div>
+								<div class="timeline-post-subtitle">
+									<p><span>Company Name </span><span class="timeline-duration">2012 - Current</span></p>
+								</div>
+								<div class="timeline-post-content">
+									<p>Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.</p>
+								</div>
+							</div> <!-- /.timeline-post-content-holder end -->							
+						</div> <!-- /.timeline-post end -->
+						<div class="timeline-post timeline-post-right">											
+							<div class="timeline-post-content-holder">
+								<div class="timeline-post-icon"></div>
+								<div class="timeline-post-title">
+									<h4>SENIOR DEVELOPER 4</h4>
+								</div>
+								<div class="timeline-post-subtitle">
+									<p><span>Company Name </span><span class="timeline-duration">2012 - Current</span></p>
+								</div>
+								<div class="timeline-post-content">
+									<p>Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.</p>
+								</div>
+							</div> <!-- /.timeline-post-content-holder end -->							
+						</div> <!-- /.timeline-post end -->
+
+				<!-- Experience Timeline  end -->
+
+				<!-- Education Timeline -->
+					<div class="timeline-category edu-cagegory">
+						<a class="large bt-timeline">Education</a>
+						<div class="timeline-category-icon">
+							<div class="iconspace"><i class="fa-book"></i></div>
+						</div> <!-- /.timeline-category-icon end -->
+					</div> <!-- /.timeline-category end -->
+						<div class="timeline-post timeline-post-left">											
+							<div class="timeline-post-content-holder">
+								<div class="timeline-post-icon"></div>
+								<div class="timeline-post-title">
+									<h4>SENIOR DEVELOPER</h4>
+								</div>
+								<div class="timeline-post-subtitle">
+									<p><span>Company Name </span><span class="timeline-duration">2012 - Current</span></p>
+								</div>
+								<div class="timeline-post-content">
+									<p>Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.</p>
+								</div>
+							</div> <!-- /.timeline-post-content-holder end -->							
+						</div> <!-- /.timeline-post end -->	
+						<div class="timeline-post timeline-post-right">											
+							<div class="timeline-post-content-holder">
+								<div class="timeline-post-icon"></div>
+								<div class="timeline-post-title">
+									<h4>SENIOR DEVELOPER</h4>
+								</div>
+								<div class="timeline-post-subtitle">
+									<p><span>Company Name </span><span class="timeline-duration">2012 - Current</span></p>
+								</div>
+								<div class="timeline-post-content">
+									<p>Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.</p>
+								</div>
+							</div> <!-- /.timeline-post-content-holder end -->							
+						</div> <!-- /.timeline-post end -->
+						<div class="timeline-post timeline-post-left">											
+							<div class="timeline-post-content-holder">
+								<div class="timeline-post-icon"></div>
+								<div class="timeline-post-title">
+									<h4>SENIOR DEVELOPER</h4>
+								</div>
+								<div class="timeline-post-subtitle">
+									<p><span>Company Name </span><span class="timeline-duration">2012 - Current</span></p>
+								</div>
+								<div class="timeline-post-content">
+									<p>Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.</p>
+								</div>
+							</div> <!-- /.timeline-post-content-holder end -->							
+						</div> <!-- /.timeline-post end -->
+
+				<!-- Education Timeline -->
+
+					<div class="timeline-end-icon"><span> <i class="fa-bookmark"></i></span></div>
+				</div>	<!-- /.timeline end -->
+			</div> <!-- /.col-md-12 end -->
+		</div>	<!-- /.row end -->
+	</div> <!-- /.container end -->						
+	
+  
+
+</div>
+
+<div id="contact" class="section contact_parallax animatedParent" data-appear-top-offset="-300">
+  
+  <form id="email_form" action="index.php#contact" onsubmit="return sendEmail();" class="animated growIn slow">
+	  <h3 align=center style="color:white;  margin-bottom: 40px">Get In Touch</h3>
+	  <input id="name" name="name" class="name" placeholder="Name" required />
+	  <input id="email" name="email" class="email" placeholder="Email" required />
+	  <textarea rows="5" cols="80" id="message" name="message" class="message" placeholder="Message" required></textarea>
+	  <input name="submit" type="submit" class="submit" value="Send the message" />
+  </form>
+  
+</div>
+</div>
+
+
+<div class="footer">
+<ul class="contactInfo">
+<li><a href="mailto:runxuanwei@gmail.com"><img class="icon" src="image/email.svg"></img></a></li>
+<li><a href="https://www.linkedin.com/in/runxuan-wei-16540358"><img class="icon" src="image/linkedin.svg"></img></a></li>
+<li><a href="https://github.com/runxuanw"><img class="icon" src="image/github.svg"></img></a></li>
+<li><a href="https://leetcode.com/runxuanw"><img class="icon" src="image/leetcode.png" style="width:50px;height:50px;"></img></a></li>
+</ul>
+<br>
+&copy 2017 Runxuan Wei
+</div>
+
+</body>
+</html>
+
+<script src="animate_it/js/css3-animate-it.js"></script>
---
+++ js/util.js
@@ -0,0 +1,126 @@
+$(document).ready(function(){
+    $(this).scrollTop(0);
+		
+	$("#myPic div.top img").hover(function() {
+	   $(this).css("opacity", "0");
+	   $("#myPic div.bottom img").stop(true).animate({backgroundColor: "white"}, 1000);
+	}, function() {
+	   $(this).css("opacity", "1");
+	   $("#myPic div.bottom img").stop(true).animate({backgroundColor: "#032F4A"}, 1000);
+	});
+	
+	$("#frontLink").hover(function() {
+	  $(this).stop(true).fadeTo(500, 0, function() {
+	    $(this).html("Hey there !");
+		$(this).fadeTo(500, 1);
+	  });
+	},
+	function() {
+	  $(this).stop(true).delay(500).fadeTo(800, 0, function() {
+	    $(this).html("Hello World..<span class='blink'>.</span>");
+		$(this).fadeTo(500, 1);
+	  });
+	}
+	);
+	
+	$(".skill_icon").click(function() {
+	     if($(this).css("filter") == "none") {
+		 $(this).css("filter", "grayscale(100%)");
+		 $(".skill_icon").each(function(i) {
+	     var toggle = $(this);
+		 setTimeout(function() {
+	       toggle.css("filter", "grayscale(100%)");
+		 }, 300*i);
+	   });
+		 }
+	     else {
+		 $(this).css("filter", "none");
+		 $(".skill_icon").each(function(i) {
+	     var toggle = $(this);
+		 setTimeout(function() {
+	       toggle.css("filter", "none");
+		 }, 300*i);
+	   });
+		 }
+	   
+	});
+	
+});
+
+function sendEmail() {
+
+    $.ajax({
+        url: "https://formspree.io/runxuanwei@gmail.com", 
+        method: "POST",
+        data: {
+			from: $("#email").val(), 
+			name: $("#name").val(), 
+			message: $("#message").val()
+		},
+        dataType: "json",
+		success: function(data) {
+			swal("Thank you!", "Your message is sent successfully!", "success");
+		},
+		error: function(err) {
+			swal("Sorry...", "An error occurred.", "error");
+		}
+    });
+	return false;
+};
+
+function changeCSS() {
+	var scroll = $(window).scrollTop();
+	
+	  if(scroll < $("#intro").offset().top*2/3) {
+          $(".navbar-default").stop(true).animate({backgroundColor: "transparent"}, 700);
+		  $(".navbar-default .navbar-nav > li > a").animate({color: "white"}, 700);
+      }
+	  else if (scroll >= $("#intro").offset().top/2-100 && scroll < $("#skill").offset().top-100) {
+        $(".navbar-default").stop(true).animate({backgroundColor: "#0D394A"}, 700);
+		$(".navbar-default .navbar-nav > li > a").animate({color: "white"}, 700);
+		$(".navbar-default").css("opacity" , "1"); 
+      }
+	  else if (scroll >= $("#skill").offset().top/2-100 && scroll < $("#project").offset().top-100) {
+        $(".navbar-default").stop(true).animate({backgroundColor: "#144da8"}, 700);
+		$(".navbar-default .navbar-nav > li > a").animate({color: "white"}, 700);
+		$(".navbar-default").css("opacity" , "1"); 
+      }
+	  else if(scroll >= $("#project").offset().top-100 && scroll < $("#resume").offset().top-100) {
+		$(".navbar-default").stop(true).animate({backgroundColor: "green"}, 700);
+		$(".navbar-default .navbar-nav > li > a").animate({color: "white"}, 700);
+		$(".navbar-default").css("opacity" , "1"); 
+	  }
+	  else if(scroll >= $("#resume").offset().top-100 && scroll < $("#contact").offset().top-100) {
+		$(".navbar-default").stop(true).animate({backgroundColor: "red"}, 700);
+		$(".navbar-default .navbar-nav > li > a").animate({color: "white"}, 700);
+		$(".navbar-default").css("opacity" , "1"); 
+	  }
+      else {
+        $(".navbar-default").stop(true).animate({backgroundColor: "transparent"}, 700);
+		$(".navbar-default .navbar-nav > li > a").animate({color: "white"}, 700);
+		$(".navbar-default").css("opacity" , "1");
+      }
+}
+
+function fadeFrontLink() {
+	  var scroll = $(window).scrollTop();
+	  var opacity = ($("#frontLink").offset().top-scroll)/$("#frontLink").offset().top;
+	  opacity = opacity >= 0 ? opacity : 0;
+	  $("#frontLink").css("opacity", opacity);
+}
+
+$(window).scroll(function() {
+  clearTimeout(changeCSS._tId);
+    changeCSS._tId= setTimeout(function(){
+        changeCSS();
+    }, 100);
+	
+	fadeFrontLink();
+});
+
+
+
+function navClick(navToId) {
+	$('html, body').animate({scrollTop: $(navToId).offset().top}, 500);
+	return false;
+}
